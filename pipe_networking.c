7420956d39b0c9f960057096f69c2579095c9472
Irvin Li <irvinli@Irvins-MacBook-Air.local>
2025-01-08 18:04:51 -0500

    server connecting (handshake) complete git push

--- pipe_networking.c
+++ pipe_networking.c
@@ -23,7 +23,32 @@ int server_setup() {
     return SYN;
 }
 
+//Performs the serverside part of the 3-way handshake when esablishing connection with client.
+//Returns SYN_ACK (Will prompt users for an input)
+char * server_side_authentication(char *to_client) {
+    int SYN = server_setup();
+    char private_pipe1[pipe_size];
+    read(SYN, private_pipe1, sizeof(private_pipe1));
+    int SYN_ACK = open(private_pipe1, O_WRONLY);
+    char message[256];
+    strcpy(message, "Testing");
+    write(SYN_ACK, message, strlen(message) + 1);
+    printf("SYN_ACK being sent to client");
+    char from_server[256];
+    read(SYN, from_server, sizeof(from_server));
+    if(strcmp(message, from_server)) {
+        printf("Success\n");
+    } else {
+        printf("Server failed to send");
+    }
+    printf("SYN_ACK recieved through WKP");
+    *to_client = from_server;
+    return to_client;
+}
 
+char * client_side_authentication(char *to_server) {
+    
+}
 void send_file_to_server(char *file_path);
 void receive_feedback_from_server();
 void handle_file_transfer(int pipe_fd, char *file_path);

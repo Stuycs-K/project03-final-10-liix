6017a201767ba1ddd68794b518a1fefd0e1b6b33
Irvin Li <irvinli@Irvins-MacBook-Air.local>
2025-01-10 15:12:59 -0500

    made the main function for server, will test server and client interaction

---
+++ server.c
@@ -0,0 +1,40 @@
+#include "pipe_networking.h"
+#include "pipe_networking.h"
+#include <signal.h>
+#include <stdlib.h>
+
+int running = 1;
+// Signal handler for SIGINT
+void handle_sigint(int sig) {
+  printf("\nServer: Received SIGINT, cleaning up...\n");
+  unlink(WKP); // Delete the WKP
+  running = 0; // Stop the server loop
+}
+
+int main() {
+  // Set up signal handling for shutdown
+  signal(SIGINT, handle_sigint);
+
+  while (running) {
+    printf("Server: Waiting for a client...\n");
+    int from_client = server_setup();
+
+    int to_client;
+    from_client = server_handshake(&to_client);
+
+    printf("Server: Client connected. Entering communication loop...\n");
+
+    char buffer[256];
+    while (read(from_client, buffer, sizeof(buffer)) > 0) {
+      printf("Server: Received from client: %s\n", buffer);
+      char sample = "Please send message";
+      write(to_client, sample, sizeof(sample)); 
+    }
+
+    printf("Server: Client disconnected.\n");
+    close(from_client); // Close upstream pipe
+    close(to_client);   // Close downstream pipe
+  }
+  printf("Server: Shutting down...\n");
+  remove(WKP);
+}
